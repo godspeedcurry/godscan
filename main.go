d2678b6247c165c84d84c054b5770b949f2eba7c
godspeedcurry <27125288+godspeedcurry@users.noreply.github.com>
2022-06-06 21:54:17 +0800

    add spider

--- main.go
+++ main.go
@@ -7,17 +7,20 @@ import (
 )
 
 var (
-	url   string
-	proxy string
+	Url   string
+	Proxy string
+	Depth int
 )
 
 func init() {
-	flag.StringVar(&url, "u", "https://www.baidu.com", "your url")
-	flag.StringVar(&proxy, "p", "", "your proxy")
+	flag.StringVar(&Url, "u", "https://www.baidu.com", "your url")
+	flag.StringVar(&Proxy, "p", "", "your proxy")
+	flag.IntVar(&Depth, "d", 1, "your search depth")
 }
 func main() {
 	flag.Parse()
-	utils.PrintFinger(url)
+	utils.PrintFinger(Url)
+
 	// var web_username = dict.Get_web_username()
 	// var device_username = dict.Get_device_password("topsec")
 	// color.Cyan("Prints text in cyan.")
--- utils/finger.go
+++ utils/finger.go
@@ -1,11 +1,14 @@
 package utils
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	mapset "github.com/deckarep/golang-set"
 	"github.com/fatih/color"
 )
 
@@ -29,8 +32,64 @@ func HttpGetServerHeader(Url string, NeedTitle bool) (string, string, error) {
 	return "", "", nil
 }
 
-func DisplayHeader(u string) {
-	ServerHeader, Title, err := HttpGetServerHeader(u, true)
+func Normalize(Path string, Url string) string {
+	u, _ := url.Parse(Url)
+	normalizeUrl := u.Scheme + "://" + u.Hostname()
+
+	if strings.Contains(Path, "javascript:") {
+		return ""
+	} else if strings.HasPrefix(Path, "http://") {
+		return Path
+	} else if strings.HasPrefix(Path, "https://") {
+		return Path
+	} else if strings.HasPrefix(Path, "./") {
+		return normalizeUrl + Path[1:]
+	} else if strings.HasPrefix(Path, "/") {
+		return normalizeUrl + Path
+	} else {
+		return normalizeUrl + "/" + Path
+	}
+}
+
+func Spider(Host string, Url string, depth int, s1 mapset.Set) (string, error) {
+	if !strings.Contains(Url, Host) {
+		fmt.Printf("======Depth %d, target %s =====\n", depth, Url)
+		s1.Add(Url)
+		return "", nil
+	} else if depth == 0 || strings.Contains(Url, ".min.js") || strings.Contains(Url, ".ico") {
+		return "", nil
+	}
+	fmt.Printf("======Depth %d, target %s =====\n", depth, Url)
+	s1.Add(Url)
+	req, _ := http.NewRequest(http.MethodGet, Url, nil)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36")
+	resp, err := Client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	doc, _ := goquery.NewDocumentFromReader(resp.Body)
+	// a标签
+	doc.Find("a").Each(func(i int, a *goquery.Selection) {
+		href, _ := a.Attr("href")
+		normalizeUrl := Normalize(href, Url)
+		if normalizeUrl != "" && !s1.Contains(normalizeUrl) {
+			Spider(Host, normalizeUrl, depth-1, s1)
+		}
+	})
+	// script 标签
+	doc.Find("script").Each(func(i int, a *goquery.Selection) {
+		src, _ := a.Attr("src")
+		normalizeUrl := Normalize(src, Url)
+		if normalizeUrl != "" && !s1.Contains(normalizeUrl) {
+			Spider(Host, normalizeUrl, depth-1, s1)
+		}
+	})
+	return "", nil
+}
+
+func DisplayHeader(Url string) {
+	ServerHeader, Title, err := HttpGetServerHeader(Url, true)
 	if err != nil {
 		color.HiRed("Error: %s\n", err)
 	} else {
@@ -38,15 +97,21 @@ func DisplayHeader(u string) {
 		color.Cyan("Title: %s\n", Title)
 	}
 }
+
 func PrintFinger(Url string) {
 	InitHttp()
 	color.HiRed("Your URL: %s\n", Url)
-	u, _ := url.Parse(Url)
+	Host, _ := url.Parse(Url)
 
-	FirstUrl := u.Scheme + "://" + u.Hostname()
+	// 首页
+	FirstUrl := Host.Scheme + "://" + Host.Hostname()
 	DisplayHeader(FirstUrl)
 
-	SecondUrl := u.Scheme + "://" + u.Hostname() + "/xxxxxx"
+	// 构造404
+	SecondUrl := Host.Scheme + "://" + Host.Hostname() + "/xxxxxx"
 	DisplayHeader(SecondUrl)
+
+	s1 := mapset.NewSet()
+	Spider(Host.Hostname(), Url, 2, s1)
 
 }
