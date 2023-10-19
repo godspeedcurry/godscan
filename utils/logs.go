9c0f57ac6222368f3d93d9d6bb002b5a1ff1777e
godspeedcurry <27125288+godspeedcurry@users.noreply.github.com>
2023-10-19 10:09:35 +0800

    code refactoring
    Former-commit-id: 28b24ffa4fd74847cfa6f5802fb96410cca07ed0

--- utils/logs.go
+++ utils/logs.go
@@ -30,30 +30,34 @@ func log_print(level int, detail string) {
 	fmt.Println(detail)
 }
 
+func LogBeautify(x string, colorAttr color.Attribute, y string) {
+	fmt.Printf("[%s] [%s] %s\n", get_current_time(), color.New(colorAttr).Sprintf("%s", x), y)
+}
+
 func Debug(format string, args ...interface{}) {
-	color.Cyan("[%s] [%s] %s", get_current_time(), "DEBUG", fmt.Sprintf(format, args...))
+	LogBeautify("DEBUG", color.BgYellow, fmt.Sprintf(format, args...))
 }
 
 func Info(format string, args ...interface{}) {
-	color.Cyan("[%s] [%s] %s", get_current_time(), "INFO", fmt.Sprintf(format, args...))
+	LogBeautify("INFO", color.FgCyan, fmt.Sprintf(format, args...))
 }
 
 func Success(format string, args ...interface{}) {
-	color.Green("[%s] [%s] %s", get_current_time(), "SUCCESS", fmt.Sprintf(format, args...))
+	LogBeautify("SUCCESS", color.FgHiRed, fmt.Sprintf(format, args...))
 }
 
 func Failed(format string, args ...interface{}) {
-	color.Green("[%s] [%s] %s", get_current_time(), "FAILED", fmt.Sprintf(format, args...))
+	LogBeautify("FAILED", color.BgRed, fmt.Sprintf(format, args...))
 }
 
 func Warning(format string, args ...interface{}) {
-	color.Yellow("[%s] [%s] %s", get_current_time(), "WARN", fmt.Sprintf(format, args...))
+	LogBeautify("WARN", color.FgYellow, fmt.Sprintf(format, args...))
 }
 
 func Error(format string, args ...interface{}) {
-	color.Red("[%s] [%s] %s", get_current_time(), "ERROR", fmt.Sprintf(format, args...))
+	LogBeautify("ERROR", color.FgRed, fmt.Sprintf(format, args...))
 }
 
 func Fatal(format string, args ...interface{}) {
-	color.HiRed("[%s] [%s] %s", get_current_time(), "FATAL", fmt.Sprintf(format, args...))
+	LogBeautify("FATAL", color.FgHiRed, fmt.Sprintf(format, args...))
 }
