40d497e933c13380b0a53c392b60fa63bf3d6593
godspeedcurry <27125288+godspeedcurry@users.noreply.github.com>
2024-04-15 11:51:30 +0800

    fix map bug
    Former-commit-id: fe15e5a776bcaf1f51461bb713c02346f0b1cab3
    Former-commit-id: 40843764cc0d7e6b5452442568cf38813bca0da5

--- utils/useful.go
+++ utils/useful.go
@@ -52,17 +52,6 @@ func (p PairList) Len() int           { return len(p) }
 func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
 func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
-func mysort(mymap map[string]int) PairList {
-	pl := make(PairList, len(mymap))
-	i := 0
-	for k, v := range mymap {
-		pl[i] = Pair{k, v}
-		i++
-	}
-	sort.Sort(sort.Reverse(pl))
-	return pl
-}
-
 func StringListToInterfaceList(tmpList []string) []interface{} {
 	vals := make([]interface{}, len(tmpList))
 	for i, v := range tmpList {
@@ -168,15 +157,6 @@ func removeDuplicatesString(arr []string) []string {
 	}
 	sort.Strings(result)
 	return result
-}
-
-func in(str_array []string, target string) bool {
-	sort.Strings(str_array)
-	index := sort.SearchStrings(str_array, target)
-	if index < len(str_array) && str_array[index] == target { //需要注意此处的判断，先判断 &&左侧的条件，如果不满足则结束此处判断，不会再进行右侧的判断
-		return true
-	}
-	return false
 }
 
 func Quote(x string) string {
