de374a39f67fee99e48b8dec57b531ac1a4536ad
godspeed <27125288+godspeedcurry@users.noreply.github.com>
2024-04-30 10:19:45 +0800

    port scanned module added

---
+++ cmd/port.go
@@ -0,0 +1,70 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/godspeedcurry/godscan/utils"
+	"github.com/spf13/viper"
+)
+
+type PortOptions struct {
+	IpRange         string
+	PortRange       string
+	useAllProbes    bool
+	nullProbeOnly   bool
+	scanSendTimeout int
+	scanReadTimeout int
+	scanRarity      int
+}
+
+var (
+	portOptions PortOptions
+)
+
+func (o *PortOptions) validateOptions() error {
+	if portOptions.IpRange == "" {
+		return fmt.Errorf("please give ips")
+	}
+	return nil
+}
+func init() {
+	ipCmd := newCommandWithAliases("port", "port scanner", []string{"pp"}, &portOptions)
+	rootCmd.AddCommand(ipCmd)
+
+	ipCmd.PersistentFlags().StringVarP(&portOptions.IpRange, "host", "i", "", "your ip list")
+	ipCmd.PersistentFlags().StringVarP(&portOptions.PortRange, "port", "p", "80", "your port list")
+
+	ipCmd.PersistentFlags().IntVarP(&portOptions.scanSendTimeout, "scan-send-timeout", "s", 5, "Set connection send timeout in seconds")
+	ipCmd.PersistentFlags().IntVarP(&portOptions.scanReadTimeout, "scan-read-timeout", "r", 5, "Set connection read timeout in seconds")
+	ipCmd.PersistentFlags().IntVarP(&portOptions.scanRarity, "scan-rarity", "R", 7, "Scan Rarity")
+
+	ipCmd.PersistentFlags().BoolVarP(&portOptions.useAllProbes, "all-probe", "a", false, "Use all probes to probe service")
+	ipCmd.PersistentFlags().BoolVarP(&portOptions.nullProbeOnly, "null-probe-only", "n", false, "Use all probes to probe service")
+
+	viper.BindPFlag("host", ipCmd.PersistentFlags().Lookup("host"))
+	viper.SetDefault("host", "")
+
+	viper.BindPFlag("port", ipCmd.PersistentFlags().Lookup("port"))
+	viper.SetDefault("port", "")
+	viper.BindPFlag("scan-rarity", ipCmd.PersistentFlags().Lookup("scan-rarity"))
+	viper.SetDefault("scan-rarity", 5)
+
+	viper.BindPFlag("scan-send-timeout", ipCmd.PersistentFlags().Lookup("scan-send-timeout"))
+	viper.SetDefault("scan-send-timeout", 5)
+
+	viper.BindPFlag("scan-read-timeout", ipCmd.PersistentFlags().Lookup("scan-read-timeout"))
+	viper.SetDefault("scan-read-timeout", 5)
+
+	viper.BindPFlag("null-probe-only", ipCmd.PersistentFlags().Lookup("null-probe-only"))
+	viper.SetDefault("null-probe-only", false)
+
+	viper.BindPFlag("all-probe", ipCmd.PersistentFlags().Lookup("all-probe"))
+	viper.SetDefault("all-probe", false)
+}
+
+func (o *PortOptions) run() {
+	utils.PortScan(portOptions.IpRange, portOptions.PortRange)
+}
---
+++ utils/port.go
@@ -0,0 +1,190 @@
+package utils
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/malfunkt/iprange"
+)
+
+type ProtocolInfo struct {
+	Ip   string
+	Port int
+}
+
+func parsePorts(portsStr string) ([]int, error) {
+	var ports []int
+
+	portRanges := strings.Split(portsStr, ",")
+	for _, portRange := range portRanges {
+		portRange = strings.TrimSpace(portRange)
+
+		if strings.Contains(portRange, "-") { // 处理端口范围
+			rangeBounds := strings.Split(portRange, "-")
+			if len(rangeBounds) != 2 {
+				return nil, fmt.Errorf("invalid port range format: %s", portRange)
+			}
+
+			startPort, err := strconv.Atoi(strings.TrimSpace(rangeBounds[0]))
+			if err != nil {
+				return nil, fmt.Errorf("invalid start port in range: %s", rangeBounds[0])
+			}
+
+			endPort, err := strconv.Atoi(strings.TrimSpace(rangeBounds[1]))
+			if err != nil {
+				return nil, fmt.Errorf("invalid end port in range: %s", rangeBounds[1])
+			}
+
+			if startPort > endPort {
+				return nil, fmt.Errorf("invalid port range: start port cannot be greater than end port")
+			}
+
+			for port := startPort; port <= endPort; port++ {
+				ports = append(ports, port)
+			}
+		} else { // 处理单个端口
+			port, err := strconv.Atoi(portRange)
+			if err != nil {
+				return nil, fmt.Errorf("invalid port: %s", portRange)
+			}
+			ports = append(ports, port)
+		}
+	}
+
+	return ports, nil
+}
+
+func ipLessThanOrEqual(a, b net.IP) bool {
+	for i := range a {
+		if a[i] < b[i] {
+			return true
+		}
+		if a[i] > b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func incrementIP(ip net.IP) net.IP {
+	nextIP := make(net.IP, len(ip))
+	copy(nextIP, ip)
+	for i := len(nextIP) - 1; i >= 0; i-- {
+		nextIP[i]++
+		if nextIP[i] > 0 {
+			break
+		}
+	}
+	return nextIP
+}
+
+// convertIPListToPool 将给定的IP列表转换为IP池
+func convertIPListToPool(ipList []string) ([]net.IP, error) {
+	var ipPool []net.IP
+
+	for _, ipStr := range ipList {
+		ipStr = strings.TrimSpace(ipStr)
+		if strings.Contains(ipStr, "-") { // 处理范围格式的IP
+			ipRange := strings.Split(ipStr, "-")
+			if len(ipRange) != 2 {
+				return nil, fmt.Errorf("invalid IP range format: %s", ipStr)
+			}
+
+			startIP := net.ParseIP(strings.TrimSpace(ipRange[0]))
+			endIP := net.ParseIP(strings.TrimSpace(ipRange[1]))
+			if startIP == nil || endIP == nil {
+				return nil, fmt.Errorf("invalid IP address in range: %s", ipStr)
+			}
+
+			for ip := startIP; ipLessThanOrEqual(ip, endIP); ip = incrementIP(ip) {
+				ipPool = append(ipPool, ip)
+			}
+		} else if strings.Contains(ipStr, "/") { // 处理CIDR格式的IP
+			ipr, err := iprange.Parse(ipStr)
+			if err != nil {
+				return nil, fmt.Errorf("Error parsing CIDR IP: %v", err)
+			}
+			ipPool = append(ipPool, ipr.Expand()...)
+		} else { // 单个IP
+			parsedIP := net.ParseIP(ipStr)
+			if parsedIP == nil {
+				return nil, fmt.Errorf("invalid IP address: %s", ipStr)
+			}
+			ipPool = append(ipPool, parsedIP)
+		}
+	}
+
+	return ipPool, nil
+}
+
+func handleWorker(ip string, ports chan int, results chan ProtocolInfo) {
+	for p := range ports {
+		address := fmt.Sprintf("%s:%d", ip, p)
+		conn, err := net.DialTimeout("tcp", address, 2*time.Second)
+		if err != nil {
+			results <- ProtocolInfo{Ip: ip, Port: -p}
+			continue
+		}
+		results <- ProtocolInfo{Ip: ip, Port: p}
+		conn.Close()
+	}
+}
+
+func PortScan(IpRange string, PortRange string) {
+	ips, err := convertIPListToPool(strings.Split(IpRange, ","))
+	if err != nil {
+		Error("%s", err)
+		return
+	}
+	ports_list, err := parsePorts(PortRange)
+	if err != nil {
+		Error("%s", err)
+		return
+	}
+
+	var allOpen []ProtocolInfo
+
+	for _, ipNet := range ips {
+		ports := make(chan int, 50)
+		results := make(chan ProtocolInfo)
+		var openSlice []ProtocolInfo
+
+		// 任务生产者-分发任务 (新起一个 goroutinue ，进行分发数据)
+		go func(arr []int) {
+			for i := 0; i < len(arr); i++ {
+				ports <- arr[i]
+			}
+		}(ports_list)
+
+		// 任务消费者-处理任务  (每一个端口号都分配一个 goroutinue ，进行扫描)
+		// 结果生产者-每次得到结果 再写入 结果 chan 中
+		for i := 0; i < cap(ports); i++ {
+			go handleWorker(ipNet.String(), ports, results)
+		}
+
+		// 结果消费者-等待收集结果 (main中的 goroutinue 不断从 chan 中阻塞式读取数据)
+		for i := 0; i < len(ports_list); i++ {
+			resPortInfo := <-results
+			if resPortInfo.Port > 0 {
+				openSlice = append(openSlice, resPortInfo)
+			}
+		}
+
+		// 关闭 chan
+		close(ports)
+		close(results)
+
+		// 输出
+		allOpen = append(allOpen, openSlice...)
+
+	}
+	for _, open := range allOpen {
+		Success("%s:%-8d Open", open.Ip, open.Port)
+	}
+	for _, open := range allOpen {
+		ScanWithIpAndPort(open.Ip, open.Port, "tcp")
+	}
+}
