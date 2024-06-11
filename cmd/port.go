bbacb1c992dee739632b88d6ec8957c66d6178ee
godspeed <27125288+godspeedcurry@users.noreply.github.com>
2024-06-12 07:00:03 +0800

    update port scanner module

--- cmd/port.go
+++ cmd/port.go
@@ -14,12 +14,14 @@ import (
 
 type PortOptions struct {
 	IpRange         string
+	IpRangeFile     string
 	PortRange       string
 	useAllProbes    bool
 	nullProbeOnly   bool
 	scanSendTimeout int
 	scanReadTimeout int
 	scanRarity      int
+	Threads         int
 }
 
 var (
@@ -27,7 +29,7 @@ var (
 )
 
 func (o *PortOptions) validateOptions() error {
-	if portOptions.IpRange == "" {
+	if portOptions.IpRange == "" && portOptions.IpRangeFile == "" {
 		return fmt.Errorf("please give ips")
 	}
 	return nil
@@ -37,6 +39,8 @@ func init() {
 	rootCmd.AddCommand(ipCmd)
 
 	ipCmd.PersistentFlags().StringVarP(&portOptions.IpRange, "host", "i", "", "your ip list")
+	ipCmd.PersistentFlags().StringVarP(&portOptions.IpRangeFile, "host-file", "I", "", "your ip list file")
+
 	ipCmd.PersistentFlags().StringVarP(&portOptions.PortRange, "port", "p", strings.Join(common.DefaultPorts, ","), "your port list")
 
 	ipCmd.PersistentFlags().IntVarP(&portOptions.scanSendTimeout, "scan-send-timeout", "s", 5, "Set connection send timeout in seconds")
@@ -46,11 +50,17 @@ func init() {
 	ipCmd.PersistentFlags().BoolVarP(&portOptions.useAllProbes, "all-probe", "a", false, "Use all probes to probe service")
 	ipCmd.PersistentFlags().BoolVarP(&portOptions.nullProbeOnly, "null-probe-only", "n", false, "Use all probes to probe service")
 
+	ipCmd.PersistentFlags().IntVarP(&portOptions.Threads, "threads", "t", 1000, "Number of threads to use, default is 1000")
+
 	viper.BindPFlag("host", ipCmd.PersistentFlags().Lookup("host"))
 	viper.SetDefault("host", "")
 
+	viper.BindPFlag("threads", ipCmd.PersistentFlags().Lookup("threads"))
+	viper.SetDefault("threads", 1000)
+
 	viper.BindPFlag("port", ipCmd.PersistentFlags().Lookup("port"))
 	viper.SetDefault("port", "")
+
 	viper.BindPFlag("scan-rarity", ipCmd.PersistentFlags().Lookup("scan-rarity"))
 	viper.SetDefault("scan-rarity", 5)
 
@@ -68,5 +78,12 @@ func init() {
 }
 
 func (o *PortOptions) run() {
+	if portOptions.IpRangeFile != "" {
+		ips := utils.FileReadLine(portOptions.IpRangeFile)
+		portOptions.IpRange = strings.Join(ips, ",")
+	} else if portOptions.IpRange == "" {
+		utils.Error("Please provide ip range or ip range file")
+		return
+	}
 	utils.PortScan(portOptions.IpRange, portOptions.PortRange)
 }
